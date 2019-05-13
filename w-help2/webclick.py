96cc40c9b68608af1d5b89b177d12218a0a13723
cx.dan <cx.dan@ubtrobot.com>
2019-05-13 16:42:55 +0800

    更新代码

--- w-help2/webclick.py
+++ w-help2/webclick.py
@@ -1,22 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
+
+
+def open_new_window(dr, url):
+    js = "window.open('{url}')".format(url=url)
+    print(js)
+    dr.execute_script(js)
+    pass
+
 
 options = Options()
 # options.binary_location = '/path/to/chrome.exe'
+
+prefs = {
+    "profile.managed_default_content_settings.images": 1,
+    "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
+    "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
+
+}
+
+options.add_experimental_option('prefs', prefs)
+
 driver = webdriver.Chrome(executable_path='D:\chromedriver_win32-74.3729\chromedriver.exe',
                           chrome_options=options)
-driver.get('http://www.baidu.com/')
-# driver.quit();
-text = driver.find_element_by_id('kw')
-text.clear()
-text.send_keys('吴玮霖')
-button = driver.find_element_by_id('su')
-# 接着还是通过find_element_by_id（）方法找到提按钮，
-button.submit()
-# driver.get('http://10.10.1.66:81/zentao/user-login.html')
-# account = driver.find_element_by_id('account')
-# account.send_keys('cx.dan1')
-# password = driver.find_element_by_name('password')
-# password.send_keys('123456')
-# button = driver.find_element_by_id('submit')
-# button.submit()
+# # test
+# driver.get('https://www.baidu.com/')
+# open_new_window(driver, "https://www.baidu.com/")
+# # test end
+
+
+driver.get('http://www2.xtdx.superchutou.com:8002/Login')
+account = driver.find_element_by_id('cardnumber')
+account.send_keys('51382219890829678X')
+password = driver.find_element_by_id('password')
+password.send_keys('xt123456')
+imgCode = driver.find_element_by_id('passimgCode')
+
+imgCodeStr = input("输入验证码：")
+imgCode.send_keys(imgCodeStr)
+button = driver.find_element_by_id('Button1')
+button.click()
+time.sleep(5)
+classOne = driver.find_element_by_xpath('//*[@id="containerafter_new_has"]/tr[1]/td[3]/a')
+time.sleep(1)
+classOne.click()
+time.sleep(5)
+print("hello")
+lessions = driver.find_elements_by_xpath(
+    '//*[@id="chapters"]/div[@class="chapter-item"]/ul[@class="video"]/li/a')
+for l in lessions:
+    print(l.get_attribute("href"))
+open_new_window(driver, lessions[0].get_attribute("href"))
